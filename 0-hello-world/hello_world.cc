0845d461034bd3ab8bb2d59e26d688a4d84c9068
Manya-bansal <manya227@stanford.edu>
2024-07-26 12:01:07 -0400

    hello world task launch

--- 0-hello-world/hello_world.cc
+++ 0-hello-world/hello_world.cc
@@ -5,9 +5,17 @@
 
 using namespace Legion;
 
-void hello_word(const Task *task, const std::vector<PhysicalRegion> &regions,
-                Context ctx, Runtime *runtime) {
+#define TOP_LEVEL_TASK_ID 0
+
+void hello_world(const Task *task, const std::vector<PhysicalRegion> &regions,
+                 Context ctx, Runtime *runtime) {
   std::cout << "Hello Legion" << std::endl;
 }
 
-int main(int argc, char **argv) { return 0; }
+int main(int argc, char **argv) {
+  Runtime::set_top_level_task_id(TOP_LEVEL_TASK_ID);
+  TaskVariantRegistrar registrar(TOP_LEVEL_TASK_ID, "hello_world");
+  Runtime::preregister_task_variant<hello_world>(registrar, "hello_word");
+
+  return Runtime::start(argc, argv);
+}
